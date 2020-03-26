55497e2f8a6180d47342f58261ed55c3fb10567f
ThangManhDo <thangmanh1996@gmail.com>
2020-03-27 00:23:29 +0700

    Find Dom Node

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 
 class App extends React.Component{
   constructor(){
     super();
-    this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
-
+    this.findDomNodeHandler = this.findDomNodeHandler.bind(this);
   };
 
-  forceUpdateHandler (){
-    this.forceUpdate();
-  };
+  findDomNodeHandler() {
+    var myDiv = document.getElementById('myDiv');
+    ReactDOM.findDOMNode(myDiv).style.color = 'red';
+ }
 
   render(){
     return(
       <div>
-        <div>
-        <button onClick = {this.forceUpdateHandler}>FORCE UPDATE</button>
-            <h4>Random number: {Math.random()}</h4>
-         </div>
+        <button onClick = {this.findDomNodeHandler}>FIND DOME NODE</button>
+        <div id = "myDiv">NODE </div>
       </div>
     )
   }
