b6738ec211d21059cd71d82d995207eda071b5ae
ThangManhDo <thangmanh1996@gmail.com>
2020-03-27 00:14:50 +0700

    Set State

--- src/App.js
+++ src/App.js
@@ -5,67 +5,28 @@ class App extends React.Component{
   constructor(){
     super();
     this.state = {
-      header: "Header from state...", 
-      content: "Content from state...",
-      data:[
-        {
-          "id":1,
-          "name":"Foo",
-          "age":"20"
-        },
-        {
-          "id":2,
-          "name":"Bar",
-          "age":"30"
-        },
-        {
-          "id":3,
-          "name":"Baz",
-          "age":"40"
-        }
-      ]
+      data: []
     }
-  }
+    this.setStateHandler  = this.setStateHandle.bind(this)
+  };
+
+  setStateHandle (){
+    var item = "setState...";
+    var myArray = this.state.data.slice();
+    myArray.push(item);
+    this.setState({data:myArray})
+  };
 
   render(){
     return(
       <div>
-        <h1>{this.props.headerProp}</h1>
-        <h2>{this.props.contentProp}</h2>
-        <Header />
-        <table>
-          <tbody>
-              {this.state.data.map((person, i) => <TableRow key = {i} data = {person} />)}
-          </tbody>
-        </table>
+        <div>
+            <button onClick = {this.setStateHandler}>SET STATE</button>
+            <h4>State Array: {this.state.data}</h4>
+         </div>
       </div>
     )
   }
-}
-
-class Header extends React.Component{
-  render(){
-    return(
-      <div>Header</div>
-    )
-  }
-}
-
-class TableRow extends React.Component{
-  render(){
-    return(
-      <tr>
-        <td>{this.props.data.id}</td>
-        <td>{this.props.data.name}</td>
-        <td>{this.props.data.age}</td>
-      </tr>
-    )
-  }
-}
-
-App.defaultProps = {
-  headerProp: "Header from props...",
-  contentProp:"Content from props..."
 }
 
 export default App;
